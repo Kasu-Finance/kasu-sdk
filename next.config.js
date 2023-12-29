c9cda4510103304651d2fc17e8d78061ac896741
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2023-12-29 17:47:41 +0100

    chore: next update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    compiler: {
-        styledComponents: true,
-    },
-};
 
-module.exports = nextConfig;
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
