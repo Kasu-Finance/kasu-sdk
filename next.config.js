927d392a39161d29a6ac8bd53eead2b4d39a4ec2
szehao <ngszehao123+github@hotmail.com>
2024-01-12 18:08:19 +0800

    update: added manifest for gnosis

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
+    async headers() {
+        return [
+            {
+                source: '/manifest.json',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://app.safe.global',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-Requested-With, content-type, Authorization',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
