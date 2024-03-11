7428d46b7cda396bfb49918432f77c94953b05a9
nikolaSolidant <nikola@solidant.io>
2024-03-11 01:05:27 +0100

    chore: utilized CustomTable component in repayments table

--- src/components/organisms/repayments/repaymentsTable/index.tsx
+++ src/components/organisms/repayments/repaymentsTable/index.tsx
@@ -1,83 +1,87 @@
+import { Box, SxProps, TableRow, Theme } from '@mui/material'
+import React from 'react'
+
+import CustomTable, {
+  CustomTableHeader,
+  Sort,
+} from '@/components/molecules/CustomTable'
 import {
-  Box,
-  Paper,
-  Table,
-  TableBody,
-  TableContainer,
-  TableRow,
-} from '@mui/material'
-import React, { useState } from 'react'
+  BorderTableCell,
+  StyledTableCell,
+  StyledTablePagination,
+  StyledTableSortLabel,
+} from '@/components/organisms/repayments/repaymentsTable/styled'
 
-import { getComparator, Order, stableSort } from '@/utils/table'
+import { RepaymentsData } from '@/app/mock-data/repayments-data'
+import { sortByDate, sortByNumber, sortByString } from '@/utils'
 
-import { StyledTableCell, StyledTablePagination } from './styled'
-import TableHead, { Data } from './TableHead'
+enum RepaymentTableSortKey {
+  SOURCE = 'source',
+  REPAYMENT_DATE = 'repaymentDate',
+  REPAYMENT_AMOUNT = 'repaymentAmount',
+}
+
+const headers: CustomTableHeader<RepaymentsData>[] = [
+  { label: 'Source', value: 'source' },
+  { label: 'Repayment Date', value: 'repaymentDate' },
+  { label: 'Repayment Amount', value: 'repaymentAmount' },
+]
+
+const tableRowStyle: SxProps<Theme> = {
+  background: 'transparent',
+  '& > *': {
+    textAlign: 'left !important',
+  },
+}
+
+const handleSort = <T extends RepaymentsData>(
+  a: T,
+  b: T,
+  sort: Sort<T>
+): number => {
+  const { key, direction } = sort
+
+  let sortingFunction: (a: T, b: T) => number
+
+  if (key === RepaymentTableSortKey.REPAYMENT_DATE) {
+    sortingFunction = sortByDate<T>(key, direction)
+  } else if (key === RepaymentTableSortKey.REPAYMENT_AMOUNT) {
+    sortingFunction = sortByNumber<T>(key, direction)
+  } else {
+    sortingFunction = sortByString<T>(key, direction)
+  }
+
+  return sortingFunction(a, b)
+}
 
 interface RepaymentsTableProps {
-  data: Data[]
+  data: RepaymentsData[]
 }
 
 const RepaymentsTable: React.FC<RepaymentsTableProps> = ({ data }) => {
-  const [order, setOrder] = useState<Order>('asc')
-  const [orderBy, setOrderBy] = useState<keyof Data>('source')
-  const [page, setPage] = useState<number>(0)
-  const [rowsPerPage, setRowsPerPage] = useState<number>(10)
-
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => {
-    const isAsc = orderBy === property && order === 'asc'
-    setOrder(isAsc ? 'desc' : 'asc')
-    setOrderBy(property)
-  }
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2, overflow: 'hidden' }}>
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby='tableTitle'
-            size='medium'
-          >
-            <TableHead
-              order={order}
-              orderBy={orderBy}
-              onRequestSort={handleRequestSort}
-            />
-            <TableBody>
-              {stableSort(data, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  return (
-                    <TableRow hover tabIndex={-1} key={`${row.id}_${index}`}>
-                      <StyledTableCell align='left'>
-                        {row.source}
-                      </StyledTableCell>
-                      <StyledTableCell align='left'>
-                        {row.repaymentDate}
-                      </StyledTableCell>
-                      <StyledTableCell align='left'>
-                        {row.repaymentAmount}
-                      </StyledTableCell>
-                    </TableRow>
-                  )
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <StyledTablePagination
-          rowsPerPageOptions={[10, 25]}
-          count={data.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={(event, newPage) => setPage(newPage)}
-          onRowsPerPageChange={(event) =>
-            setRowsPerPage(parseInt(event.target.value, 10))
-          }
-        />
-      </Paper>
+    <Box>
+      <CustomTable
+        ariaLabel='Repayments Table'
+        data={data}
+        headers={headers}
+        defaultSortKey={RepaymentTableSortKey.SOURCE}
+        handleSort={handleSort}
+        tableRowStyle={tableRowStyle}
+        TableCellComp={BorderTableCell}
+        SortLabelComp={StyledTableSortLabel}
+        PaginationComp={StyledTablePagination}
+      >
+        {(sortedData) =>
+          sortedData.map((row) => (
+            <TableRow key={`${row.id}_${row.source}`}>
+              <StyledTableCell>{row.source}</StyledTableCell>
+              <StyledTableCell>{row.repaymentDate}</StyledTableCell>
+              <StyledTableCell>$ {row.repaymentAmount}</StyledTableCell>
+            </TableRow>
+          ))
+        }
+      </CustomTable>
     </Box>
   )
 }
--- src/components/organisms/repayments/repaymentsTable/styled.ts
+++ src/components/organisms/repayments/repaymentsTable/styled.ts
@@ -1,62 +1,66 @@
-import { styled, TableCell, TablePagination, TableSortLabel } from "@mui/material"
+import {
+  styled,
+  TableCell,
+  TablePagination,
+  TableSortLabel,
+} from '@mui/material'
 
 export const BorderTableCell = styled(TableCell)(({ theme }) => ({
-    position: 'relative',
-    '&:not(:last-child)': {
-      '&::after': {
-        content: '""',
-        position: 'absolute',
-        right: 0,
-        top: '1.5rem',
-        bottom: '1.2rem',
-        width: '2px',
-        backgroundColor: theme.palette.grey[300],
-      },
+  position: 'relative',
+  '&:not(:last-child)': {
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      right: 0,
+      top: '1.5rem',
+      bottom: '1.2rem',
+      width: '2px',
+      backgroundColor: theme.palette.grey[300],
     },
-  }))
-  
-  export const StyledTableSortLabel = styled(TableSortLabel)(({ theme }) => ({
+  },
+}))
+
+export const StyledTableSortLabel = styled(TableSortLabel)(({ theme }) => ({
+  '& .MuiTableSortLabel-icon': {
+    color: theme.palette.icon.primary,
+    '&:hover': {
+      color: theme.palette.icon.primary,
+    },
+  },
+  '&:hover': {
+    color: 'inherit',
     '& .MuiTableSortLabel-icon': {
       color: theme.palette.icon.primary,
-      '&:hover': {
-        color: theme.palette.icon.primary,
-      },
     },
-    '&:hover': {
-      color: 'inherit',
-      '& .MuiTableSortLabel-icon': {
-        color: theme.palette.icon.primary,
-      },
+  },
+  '&.Mui-active': {
+    '& .MuiTableSortLabel-icon': {
+      color: theme.palette.icon.primary,
     },
-    '&.Mui-active': {
-      '& .MuiTableSortLabel-icon': {
-        color: theme.palette.icon.primary,
-      },
-    },
-  }))
-  
-  export const StyledTableCell = styled(TableCell)(() => ({
-    width: '33.33%',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  }))
-  
-  export const StyledTablePagination = styled(TablePagination)(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'flex-end',
-    border: 'none',
-  
-    '.MuiTablePagination-selectLabel': {
-      marginTop: '0.5em',
-      marginRight: '-0.5em',
-      color: theme.palette.grey[400],
-    },
-    '.MuiSelect-icon': {
-      marginTop: '-2px',
-    },
-    '.MuiTablePagination-displayedRows': {
-      color: theme.palette.common.black,
-      marginBottom: '1rem',
-    },
-  }))
-  
+  },
+}))
+
+export const StyledTableCell = styled(TableCell)(() => ({
+  width: '33.33%',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+}))
+
+export const StyledTablePagination = styled(TablePagination)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  border: 'none',
+
+  '.MuiTablePagination-selectLabel': {
+    marginTop: '0.5em',
+    marginRight: '-0.5em',
+    color: theme.palette.grey[400],
+  },
+  '.MuiSelect-icon': {
+    marginTop: '-2px',
+  },
+  '.MuiTablePagination-displayedRows': {
+    color: theme.palette.common.black,
+    marginBottom: '1rem',
+  },
+}))
