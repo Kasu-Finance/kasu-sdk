f6903b5e803be8a2fa22a333e764d41b50f1cee3
nikolaSolidant <nikola@solidant.io>
2024-03-13 15:47:38 +0100

    chore: move some pool details types from frontend to sdk

--- src/types.ts
+++ src/types.ts
@@ -131,3 +131,16 @@ export interface TrancheData {
     minimumDeposit: number;
     poolCapacity: number;
 }
+
+export interface PoolMetric {
+    id: string
+    content: string | number | string[]
+    unit?: string
+    isRating?: boolean
+  }
+  
+  export interface PoolDetailSection {
+    id: string
+    metrics: PoolMetric[]
+  }
+  
