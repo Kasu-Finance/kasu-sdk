6cd7f67dc80d01cef3691759049686bd7fd8173d
szehao <ngszehao123+github@hotmail.com>
2024-03-13 23:33:10 +0800

    chore: update formatDuration import

--- src/utils/formats/formatDuration.ts
+++ src/utils/formats/formatDuration.ts
@@ -1,23 +1,22 @@
-
-
-import dayjs from 'dayjs';
+import dayjs from '@/dayjs'
 
 const formatDuration = (timestamp: number) => {
-  const date = dayjs.unix(timestamp);
+  const date = dayjs.unix(timestamp)
 
-  const now = dayjs();
-  const diff = now.diff(date);
-  const duration = dayjs.duration(diff);
+  const now = dayjs()
 
-  const parts = [];
-  if (duration.years() > 0) parts.push(`${duration.years()} year${duration.years() > 1 ? 's' : ''}`);
-  if (duration.months() > 0) parts.push(`${duration.months()} month${duration.months() > 1 ? 's' : ''}`);
+  const diff = now.diff(date)
 
-  return parts.join(' • ');
-};
+  const duration = dayjs.duration(diff)
 
+  const parts = []
 
+  if (duration.years() > 0)
+    parts.push(`${duration.years()} year${duration.years() > 1 ? 's' : ''}`)
+  if (duration.months() > 0)
+    parts.push(`${duration.months()} month${duration.months() > 1 ? 's' : ''}`)
 
+  return parts.join(' • ')
+}
 
-
-export default formatDuration;
+export default formatDuration
