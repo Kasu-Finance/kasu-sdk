678e94f28e9894246569775e06b7bfc145c90db9
szehao <ngszehao123+github@hotmail.com>
2024-03-13 23:32:50 +0800

    chore: update contract addresses

--- src/config/sdk/testnet.config.ts
+++ src/config/sdk/testnet.config.ts
@@ -1,12 +1,12 @@
 import { SdkConfig } from 'kasu-sdk/src/sdk-config'
 
-export const USDC = '0x5Da3EA3A64c731ab74aC12D1593D454769eA0252'
+export const USDC = '0x1Da2378f507FF1dD5E61801453C07bF614e214d4'
 
 export const sdkConfig: SdkConfig = {
   contracts: {
-    IKSULockBonus: '0x068a06E7c9d864E77d8c65415407e948eB1A49d9',
-    IKSULocking: '0x2235E1389cC37bdC8086Faeb3abEa3d206fe8E3d',
-    KSUToken: '0x97EC1504E3a92BfAff98D25fE11F0fa4Ae08533b',
+    IKSULockBonus: '0x17Db2d008eA35A935DEe331a1501582c57Ac609B',
+    IKSULocking: '0xD61E549E7a269171Efca755577e3b7959915cA21',
+    KSUToken: '0x8A883a8Efb45AC1A2873287712010a7d95f52975',
   },
   subgraphUrl:
     'https://api.studio.thegraph.com/query/63245/kasu-wip-sepolia/version/latest',
