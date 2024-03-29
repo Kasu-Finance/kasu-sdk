1c5879466356d3e3a92a44d3b65bfced308d2e7b
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2024-03-29 18:27:23 +0100

    chore: minor adjustments

--- src/components/molecules/locking/LoyaltyOverview/LendingLoyalityInfo.tsx
+++ src/components/molecules/locking/LoyaltyOverview/LendingLoyalityInfo.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
-import useModalState from '@/hooks/context/useModalState'
 import useLoyaltyLevel from '@/hooks/locking/useLoyaltyLevel'
 import useTranslation from '@/hooks/useTranslation'
 import useEarnedRKsu from '@/hooks/web3/useEarnedRKsu'
@@ -14,14 +13,10 @@ import TokenAmount from '@/components/atoms/TokenAmount'
 import ToolTip from '@/components/atoms/ToolTip'
 import LoyaltyProgress from '@/components/molecules/locking/LoyaltyOverview/LoyaltyProgress'
 
-import { LockIcon } from '@/assets/icons'
-
 import { capitalize, formatAmount } from '@/utils'
 
 const LendingLoyalityInfo = () => {
   const { t } = useTranslation()
-  const { openModal } = useModalState()
-  const handleOpenLockingKSU = () => openModal({ name: 'lockModal' })
 
   const { rKsuAmount } = useEarnedRKsu()
   const stakedPercentage = useLockingPercentage()
@@ -64,14 +59,6 @@ const LendingLoyalityInfo = () => {
           }
         />
       </ColoredBox>
-
-      <Button
-        onClick={handleOpenLockingKSU}
-        variant='contained'
-        startIcon={<LockIcon />}
-      >
-        {t('general.lock')} KSU
-      </Button>
     </>
   )
 }
