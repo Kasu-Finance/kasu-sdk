39d9d08ebdb1e0c453e57a578dc26cfc519d6ba0
nikola <nikola@solidant.io>
2024-04-17 17:30:23 +0200

    feat: render tranches on pool cards

--- src/components/molecules/PoolCard/PoolCardContent.tsx
+++ src/components/molecules/PoolCard/PoolCardContent.tsx
@@ -1,19 +1,46 @@
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 import { PoolOverview } from '@solidant/kasu-sdk/src/services/DataService/types'
+import { useMemo } from 'react'
 
-import useTranslation from '@/hooks/useTranslation'
+import MetricWithSuffix from '@/components/atoms/MetricWithSuffix'
 
 interface PoolCardContentProps {
   pool: PoolOverview
 }
 
 const PoolCardContent: React.FC<PoolCardContentProps> = ({ pool }) => {
-  const { t } = useTranslation()
+  const { tranches, isMultiTranche } = useMemo(() => {
+    return {
+      tranches: pool?.tranches || [],
+      isMultiTranche: pool?.tranches.length > 1,
+      poolApy: pool?.apy || '0.00',
+    }
+  }, [pool])
+
   return (
     <Box display='flex' justifyContent='center' mt={3}>
-      {/* PoolCardContent  */}
-      {/* Metrics */}
-      <Typography variant='h5'>{pool.poolName}</Typography>
+      {tranches.map((tranche) => {
+        const titleKey = isMultiTranche
+          ? `lending.shortcutTranche.${tranche.name.toLowerCase()}.title`
+          : 'general.poolApy'
+        const trancheApy = parseFloat(tranche.apy) * 100
+        const formattedApy = trancheApy.toFixed(2)
+
+        return (
+          // TODO: use here <InfoColumn key={tranche.id} title={title} subtitle={} metric={tranche.apy} />
+          <MetricWithSuffix
+            key={tranche.id}
+            content={formattedApy}
+            suffix='%'
+            titleKey={titleKey}
+            tooltipKey=''
+            containerSx={{
+              width: isMultiTranche ? '50%' : '100%',
+              pb: 1,
+            }}
+          />
+        )
+      })}
     </Box>
   )
 }
