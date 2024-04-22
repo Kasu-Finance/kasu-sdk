c5bec72540b7f65863ad9096af5b9e1e1797507c
Andrea Scotto Di Minico <andrea@solidant.io>
2024-04-22 17:35:15 +0200

    Revert "chore: testing custom next confgi"
    This reverts commit 189912397b41d015093bd166f8e91b0c682ba1d8.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,60 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+// eslint-disable-next-line
+const path = require('path')
+
+const nextConfig = {
+  distDir: 'build',
+  images: {
+    domains: [
+      'example2.com', // TODO: Remove 'example2.com' and add the real URL domain here when the backend is ready.
+      'kasu.finance',
+      '',
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/lending',
+        permanent: true,
+      },
+    ]
+  },
+  experimental: {
+    optimizePackageImports: ['dayjs', 'ethers'],
+    // needed for turbo dev mode for locally linked packages
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://app.safe.global',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
