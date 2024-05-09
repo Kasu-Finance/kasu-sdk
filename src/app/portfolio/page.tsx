573887a40efba113eccb610f4d75b4142d08b437
szehao <ngszehao123+github@hotmail.com>
2024-05-09 17:07:29 +0800

    feat: added redirect for portfolio when account is not connected

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import { Container, Stack } from '@mui/material'
+import { useWeb3React } from '@web3-react/core'
+import { redirect } from 'next/navigation'
 
 import PortfolioSummary from '@/components/organisms/portfolio/PortfolioSummary'
 import PortfolioTabs from '@/components/organisms/portfolio/PortfolioTabs'
 
+import { Routes } from '@/config/routes'
+
 const Portfolio = () => {
+  const { account } = useWeb3React()
+
+  if (!account) redirect(Routes.home.root.url)
+
   return (
     <Container maxWidth='lg'>
       <Stack spacing={3}>
