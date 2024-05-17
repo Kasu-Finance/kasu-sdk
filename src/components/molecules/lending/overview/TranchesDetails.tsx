b91af01d015ef79b73608b29d1b533a088bb02b0
Andrea Scotto Di Minico <andrea@solidant.io>
2024-05-17 15:56:41 +0200

    refactor: simplified components

--- src/components/molecules/lending/overview/TranchesDetails.tsx
+++ src/components/molecules/lending/overview/TranchesDetails.tsx
@@ -1,43 +1,77 @@
-'use client'
-
 import { Card, CardContent, Grid } from '@mui/material'
-import { PoolOverview } from '@solidant/kasu-sdk/src/services/DataService/types'
+import {
+  PoolOverview,
+  TrancheData,
+} from '@solidant/kasu-sdk/src/services/DataService/types'
+import React, { useMemo } from 'react'
 
 import TranchDetailCard from '@/components/molecules/TranchDetailCard'
 
 import { COLS } from '@/constants'
 import { formatAmount, sortTranches } from '@/utils'
 
-const TranchesDetails: React.FC<{ pool: PoolOverview }> = ({ pool }) => {
-  const sortedTranches = sortTranches(pool.tranches)
+interface TranchesDetailsProps {
+  pool: PoolOverview
+}
+
+interface TrancheCardProps {
+  tranche: TrancheData
+  isSingleTranche: boolean
+  columnWidth: number
+}
+
+const TrancheCard: React.FC<TrancheCardProps> = ({
+  tranche,
+  isSingleTranche,
+  columnWidth,
+}) => {
+  const title = isSingleTranche ? '' : `${tranche.name} Tranche`
+  const remainingAmountPct = formatAmount(
+    parseFloat(tranche.poolCapacityPercentage || '0') * 100
+  )
+  const remainingAmountValue = formatAmount(tranche.poolCapacity || '0')
+  const minimumDepositAmount = formatAmount(tranche.minimumDeposit || '0')
+  const maxDepositAmount = formatAmount(tranche.maximumDeposit || '0')
+
+  return (
+    <Grid item xs={columnWidth} key={tranche.id}>
+      <TranchDetailCard
+        title={title}
+        isSingleTranche={isSingleTranche}
+        remainingAmount={{
+          pct: remainingAmountPct,
+          value: remainingAmountValue,
+        }}
+        minimumDepositAmount={minimumDepositAmount}
+        maxDepositAmount={maxDepositAmount}
+      />
+    </Grid>
+  )
+}
+
+const TranchesDetails: React.FC<TranchesDetailsProps> = ({ pool }) => {
+  const sortedTranches = useMemo(
+    () => sortTranches(pool.tranches as TrancheData[]),
+    [pool.tranches]
+  )
+  const trancheCount = sortedTranches.length
+  const columnWidth = COLS / trancheCount
+  const isSingleTranche = trancheCount === 1
 
   return (
     <Card>
-      <Grid container columnSpacing={3} rowGap={2} component={CardContent}>
-        {sortedTranches.length > 0 &&
-          sortedTranches.map((tranche, index) => {
-            const isSingleTranche = sortedTranches.length === 1
-
-            return (
-              <Grid item xs={COLS / pool.tranches.length} key={index}>
-                <TranchDetailCard
-                  title={isSingleTranche ? '' : tranche.name + ' Tranche'}
-                  isSingleTranche={isSingleTranche}
-                  remainingAmount={{
-                    pct: formatAmount(
-                      parseFloat(tranche.poolCapacityPercentage || '0') * 100
-                    ),
-                    value: formatAmount(tranche.poolCapacity || '0'),
-                  }}
-                  minimumDepositAmount={formatAmount(
-                    tranche.minimumDeposit || '0'
-                  )}
-                  maxDepositAmount={formatAmount(tranche.maximumDeposit || '0')}
-                />
-              </Grid>
-            )
-          })}
-      </Grid>
+      <CardContent>
+        <Grid container columnSpacing={3} rowGap={2}>
+          {sortedTranches.map((tranche) => (
+            <TrancheCard
+              key={tranche.id}
+              tranche={tranche}
+              isSingleTranche={isSingleTranche}
+              columnWidth={columnWidth}
+            />
+          ))}
+        </Grid>
+      </CardContent>
     </Card>
   )
 }
