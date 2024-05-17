fe27c5ea7e8319dc17d60a3b3eaa41c48667ad45
szehao <ngszehao123+github@hotmail.com>
2024-05-17 14:08:07 +0800

    update: change icon and text for KycButton

--- src/components/atoms/KycButton.tsx
+++ src/components/atoms/KycButton.tsx
@@ -1,9 +1,11 @@
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet'
 import { Button, ButtonProps } from '@mui/material'
 import { useWeb3React } from '@web3-react/core'
 import { ReactNode } from 'react'
 
 import useKycState from '@/hooks/context/useKycState'
 import useModalState from '@/hooks/context/useModalState'
+import useTranslation from '@/hooks/useTranslation'
 
 import { ModalsKeys } from '@/context/modal/modal.types'
 
@@ -16,12 +18,16 @@ type KycButtonProps = ButtonProps & {
 }
 
 const KycButton: React.FC<KycButtonProps> = (props) => {
+  const { t } = useTranslation()
+
   const { account } = useWeb3React()
   const { openModal } = useModalState()
   const { isVerifying, isAuthenticated, kycCompleted } = useKycState()
 
   const getButtonState = (): {
     children: ReactNode
+    startIcon?: ReactNode
+    endIcon?: ReactNode
     onClick: React.MouseEventHandler<HTMLButtonElement> | undefined
     disabled: boolean
   } => {
@@ -29,7 +35,9 @@ const KycButton: React.FC<KycButtonProps> = (props) => {
 
     if (!account) {
       return {
-        children: defaultText?.connectWallet ?? 'Connect Wallet',
+        children: defaultText?.connectWallet ?? t('general.connectWallet'),
+        startIcon: <AccountBalanceWalletIcon />,
+        endIcon: undefined,
         onClick: () => openModal({ name: ModalsKeys.CONNECT_WALLET }),
         disabled: false,
       }
