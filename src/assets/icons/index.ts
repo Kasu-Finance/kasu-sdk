3e8d9599cc8344436d0eff3242c8366894792736
szehao <ngszehao123+github@hotmail.com>
2024-05-20 12:31:28 +0800

    chore: added icons and logos

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,5 @@
 export { default as CoinbaseIcon } from './connectors/CoinbaseIcon'
 export { default as MetamaskIcon } from './connectors/MetamaskIcon'
 export { default as WalletConnectIcon } from './connectors/WalletConnectIcon'
+export { default as EtherIcon } from './tokens/EtherIcon'
+export { default as UsdcIcon } from './tokens/UsdcIcon'
---
+++ src/assets/icons/tokens/EtherIcon.tsx
@@ -0,0 +1,38 @@
+const EtherIcon = () => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <circle cx='12' cy='12' r='12' fill='#C4996C' />
+    <path
+      d='M12.3893 3L12.2715 3.40013V9.50195L17.7783 11.942L12.3893 3Z'
+      fill='white'
+      fillOpacity='0.8'
+    />
+    <path d='M12.3892 3L7 11.942L12.3892 9.4924V3Z' fill='white' />
+    <path
+      d='M12.3896 16.1477L12.3232 16.2287V20.3643L12.3896 20.5581L17.782 12.9639L12.3896 16.1477Z'
+      fill='white'
+      fillOpacity='0.8'
+    />
+    <path
+      d='M12.3892 20.5581V16.1477L7 12.9639L12.3892 20.5581Z'
+      fill='white'
+    />
+    <path
+      d='M12.3896 15.1273L17.7787 11.9418L12.3896 9.49219V15.1273Z'
+      fill='white'
+      fillOpacity='0.6'
+    />
+    <path
+      d='M7 11.9418L12.3892 15.1273V9.49219L7 11.9418Z'
+      fill='white'
+      fillOpacity='0.8'
+    />
+  </svg>
+)
+
+export default EtherIcon
