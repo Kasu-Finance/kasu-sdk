beff5c60f5b28999ee1feafb2db5bd45adcd0bc0
szehao <ngszehao123+github@hotmail.com>
2024-05-23 15:49:53 +0800

    chore: change formatPercentage 0 value return to 2 decimals

--- src/utils/formats/formatPercentage.ts
+++ src/utils/formats/formatPercentage.ts
@@ -2,7 +2,7 @@ const formatPercentage = (value: string | number) => {
   const numericValue = typeof value === 'string' ? parseFloat(value) : value
 
   if (numericValue === 0) {
-    return '0 %'
+    return '0.00 %'
   }
 
   return `${(numericValue * 100).toFixed(2)} %`
