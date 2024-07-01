ee2edc25a8ba0c97a7dad2ee7755101fb7d15d36
Andrea Scotto Di Minico <andrea@solidant.io>
2024-07-01 17:33:19 +0200

    refactor: pools api route

--- src/app/api/pools/route.ts
+++ src/app/api/pools/route.ts
@@ -1,32 +1,50 @@
+import { PoolOverview } from '@solidant/kasu-sdk/src/services/DataService/types'
 import { unstable_cache } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { getKasuSDK } from '@/server/getKasuSDK.server'
 
-export const dynamic = 'force-dynamic'
-
 const sdk = getKasuSDK()
+
+const API_ROUTE_TTL = 60 * 60 // 1 hour
+const CACHE_TTL = 60 * 60 // 1 hour
 
 const getPoolOverview = unstable_cache(
   async (poolId?: string) => {
     const arg = poolId ? [poolId] : undefined
-
     return await sdk.DataService.getPoolOverview(arg)
   },
-  ['poolOverview']
+  ['poolOverview'],
+  {
+    // use it to revalidate/flush cache with revalidateTag()
+    tags: ['pools'],
+    revalidate: CACHE_TTL,
+  }
 )
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  const { searchParams } = new URL(req.url)
 
   const id = searchParams.getAll('id').join(',')
 
-  const pools = await getPoolOverview(id)
+  try {
+    const pools: PoolOverview[] = await getPoolOverview(id)
 
-  const response = NextResponse.json({ poolOverview: pools }, { status: 200 })
+    const response = NextResponse.json({ poolOverview: pools }, { status: 200 })
 
-  // Set Cache-Control headers for 1 minute
-  response.headers.set('Cache-Control', 'public, max-age=600, s-maxage=600')
+    response.headers.set(
+      'Cache-Control',
+      `public, max-age=${API_ROUTE_TTL}, s-maxage=${API_ROUTE_TTL}`
+    )
 
-  return response
+    return response
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occurred while fetching data',
+        error: (error as Error).message,
+      },
+      { status: 500 }
+    )
+  }
 }
