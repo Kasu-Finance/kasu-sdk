583107eabbf6eb5069c02043d37a2f38b3bc897c
szehao <ngszehao123+github@hotmail.com>
2024-09-01 19:51:30 +0800

    update: bad debts directus structure change

--- src/components/molecules/risk/BadDebtsTable/DataTypography.tsx
+++ src/components/molecules/risk/BadDebtsTable/DataTypography.tsx
@@ -2,27 +2,33 @@ import { Typography, TypographyProps } from '@mui/material'
 
 import ToolTip from '@/components/atoms/ToolTip'
 
+import { formatAmount } from '@/utils'
+
 interface DataTypographyProps extends TypographyProps {
   data: number | string | null
   suffix?: string
   toolTip?: string
+  isLabel?: boolean
 }
 
 const DataTypography: React.FC<DataTypographyProps> = ({
   data,
   suffix,
   toolTip,
+  isLabel,
   ...typographyProps
 }) => {
+  const isValidData = typeof data === 'number' && !isNaN(data)
+
   return (
     <Typography
       {...typographyProps}
-      color={data === 'N/A' ? 'grey.400' : 'inherit'}
+      color={!isValidData && !isLabel ? 'grey.400' : 'inherit'}
     >
-      {data}
-      {suffix && (
+      {isValidData ? formatAmount(data) : isLabel ? data : 'N/A'}
+      {suffix && isValidData && (
         <Typography variant='caption' component='span'>
-          {suffix}
+          &nbsp;{suffix}
         </Typography>
       )}
       {toolTip && (
