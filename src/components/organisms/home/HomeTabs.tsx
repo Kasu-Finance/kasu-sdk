80b5417ca05fcf95273418001e69746a9324dfce
szehao <ngszehao123+github@hotmail.com>
2024-12-09 16:41:18 +0800

    update: make tabs sticky on scroll

--- src/components/organisms/home/HomeTabs.tsx
+++ src/components/organisms/home/HomeTabs.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { Tabs } from '@mui/material'
+import { Box, Container, Tabs } from '@mui/material'
 import { usePathname } from 'next/navigation'
+import { useRef } from 'react'
 
+import useIsSticky from '@/hooks/useIsSticky'
 import getTranslation from '@/hooks/useTranslation'
 
 import CustomTab from '@/components/atoms/CustomTab'
@@ -12,32 +14,58 @@ import DisplayOptions from '@/components/organisms/home/DisplayOptions'
 import { Routes } from '@/config/routes'
 
 const HomeTabs = () => {
+  const ref = useRef(null)
+
   const { t } = getTranslation()
 
   const pathName = usePathname()
 
+  const { isSticky } = useIsSticky({
+    elementRef: ref,
+    threshold: 84,
+  })
+
   return (
-    <TabsContainer>
-      <Tabs
-        value={pathName}
-        TabIndicatorProps={{
-          sx: { bgcolor: 'gray.extraDark', height: 4, borderRadius: 1 },
-        }}
+    <>
+      {/* Adds filler space when container becomes fixed and becomes "missing" space from the DOM */}
+      {isSticky && <Box pb={6} />}
+      <Container
+        maxWidth='lg'
+        ref={ref}
+        style={{ padding: isSticky ? undefined : 0 }}
         sx={{
-          width: 'max-content',
+          bgcolor: 'white',
+          ...(isSticky && {
+            position: 'fixed',
+            top: 84, // navbar height
+            zIndex: 1000,
+            transform: 'translateX(-24px)',
+          }),
         }}
       >
-        <CustomTab
-          label={t('home.tabs.activePools')}
-          value={Routes.lending.root.url}
-        />
-        <CustomTab
-          label={t('home.tabs.closedPools')}
-          value={Routes.lending.closedLendingStrategies.url}
-        />
-      </Tabs>
-      <DisplayOptions />
-    </TabsContainer>
+        <TabsContainer>
+          <Tabs
+            value={pathName}
+            TabIndicatorProps={{
+              sx: { bgcolor: 'gray.extraDark', height: 4, borderRadius: 1 },
+            }}
+            sx={{
+              width: 'max-content',
+            }}
+          >
+            <CustomTab
+              label={t('home.tabs.activePools')}
+              value={Routes.lending.root.url}
+            />
+            <CustomTab
+              label={t('home.tabs.closedPools')}
+              value={Routes.lending.closedLendingStrategies.url}
+            />
+          </Tabs>
+          <DisplayOptions />
+        </TabsContainer>
+      </Container>
+    </>
   )
 }
 
--- src/components/organisms/portfolio/PortfolioTabs.tsx
+++ src/components/organisms/portfolio/PortfolioTabs.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { Tabs } from '@mui/material'
+import { Box, Container, Tabs } from '@mui/material'
 import { usePathname } from 'next/navigation'
-import { memo } from 'react'
+import { memo, useRef } from 'react'
 
+import useIsSticky from '@/hooks/useIsSticky'
 import getTranslation from '@/hooks/useTranslation'
 
 import CustomTab from '@/components/atoms/CustomTab'
@@ -12,39 +13,65 @@ import TabsContainer from '@/components/atoms/TabsContainer'
 import { Routes } from '@/config/routes'
 
 const PortfolioTabs = () => {
+  const ref = useRef(null)
+
   const { t } = getTranslation()
 
   const pathName = usePathname()
 
+  const { isSticky } = useIsSticky({
+    elementRef: ref,
+    threshold: 84,
+  })
+
   return (
-    <TabsContainer>
-      <Tabs
-        value={pathName}
-        TabIndicatorProps={{
-          sx: { bgcolor: 'gray.extraDark', height: 4, borderRadius: 1 },
-        }}
+    <>
+      {/* Adds filler space when container becomes fixed and becomes "missing" space from the DOM */}
+      {isSticky && <Box pb={6} />}
+      <Container
+        maxWidth='lg'
+        ref={ref}
+        style={{ padding: isSticky ? undefined : 0 }}
         sx={{
-          width: 'max-content',
+          bgcolor: 'white',
+          ...(isSticky && {
+            position: 'fixed',
+            top: 84, // navbar height
+            zIndex: 1000,
+            transform: 'translateX(-24px)',
+          }),
         }}
       >
-        <CustomTab
-          label={t('portfolio.tabs.lendingPortflio')}
-          value={Routes.portfolio.root.url}
-        />
-        <CustomTab
-          label={t('portfolio.tabs.yourTransactions')}
-          value={Routes.portfolio.yourTransactions.url}
-        />
-        <CustomTab
-          label={t('portfolio.tabs.bonusAndRewards')}
-          value={Routes.portfolio.rewards.url}
-        />
-        <CustomTab
-          label={t('general.wallet')}
-          value={Routes.portfolio.wallet.url}
-        />
-      </Tabs>
-    </TabsContainer>
+        <TabsContainer>
+          <Tabs
+            value={pathName}
+            TabIndicatorProps={{
+              sx: { bgcolor: 'gray.extraDark', height: 4, borderRadius: 1 },
+            }}
+            sx={{
+              width: 'max-content',
+            }}
+          >
+            <CustomTab
+              label={t('portfolio.tabs.lendingPortflio')}
+              value={Routes.portfolio.root.url}
+            />
+            <CustomTab
+              label={t('portfolio.tabs.yourTransactions')}
+              value={Routes.portfolio.yourTransactions.url}
+            />
+            <CustomTab
+              label={t('portfolio.tabs.bonusAndRewards')}
+              value={Routes.portfolio.rewards.url}
+            />
+            <CustomTab
+              label={t('general.wallet')}
+              value={Routes.portfolio.wallet.url}
+            />
+          </Tabs>
+        </TabsContainer>
+      </Container>
+    </>
   )
 }
 
