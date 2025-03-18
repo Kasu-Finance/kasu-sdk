6a12b1e9c9707feb788ca02f4f4bf865022326c0
szehao <ngszehao123+github@hotmail.com>
2025-03-18 17:19:18 +0800

    update: puppeteer configuration fix attempt

--- src/actions/downloadLoanContract.ts
+++ src/actions/downloadLoanContract.ts
@@ -1,17 +1,24 @@
 'use server'
 
-import { launch } from 'puppeteer'
+import { launch } from 'puppeteer-core'
 
 const downloadLoanContract = async (contract: string) => {
-  const browser = await launch()
+  const browser = await launch({
+    headless: true,
+    args: [
+      `--no-sandbox`,
+      `--headless`,
+      `--disable-gpu`,
+      `--disable-dev-shm-usage`,
+    ],
+    channel: 'chrome',
+  })
 
   const page = await browser.newPage()
 
   page.setContent(`<html><body>${contract}</body></html>`)
 
-  const pdfBlob = await page.pdf({
-    path: 'test.pdf',
-  })
+  const pdfBlob = await page.pdf()
 
   return pdfBlob
 }
