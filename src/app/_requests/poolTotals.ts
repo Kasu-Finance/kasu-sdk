29a001ee62cd4484694944e0c33d5ec333d94d17
szehao <ngszehao123+github@hotmail.com>
2025-04-02 22:53:37 +0800

    update: remove cache for getPoolsTotals

--- src/app/_requests/poolTotals.ts
+++ src/app/_requests/poolTotals.ts
@@ -1,20 +1,18 @@
 import { PoolOverview } from '@solidant/kasu-sdk/src/services/DataService/types'
-import { unstable_cache } from 'next/cache'
 
 import { getKasuSDK } from '@/actions/getKasuSDK'
 
-const CACHE_TTL = 60 * 60 // 1 hour
+// const CACHE_TTL = 1
 
-export const getPoolsTotals = unstable_cache(
-  async (poolOverviews: PoolOverview[]) => {
-    const sdk = await getKasuSDK()
+export const getPoolsTotals = async (poolOverviews: PoolOverview[]) => {
+  const sdk = await getKasuSDK()
 
-    return await sdk.DataService.getLendingTotals(poolOverviews)
-  },
-  ['totals'],
-  {
-    // use it to revalidate/flush cache with revalidateTag()
-    tags: ['totals'],
-    revalidate: CACHE_TTL,
-  }
-)
+  return await sdk.DataService.getLendingTotals(poolOverviews)
+}
+// ['totals'],
+// {
+//   // use it to revalidate/flush cache with revalidateTag()
+//   tags: ['totals'],
+//   revalidate: CACHE_TTL,
+// }
+// )
