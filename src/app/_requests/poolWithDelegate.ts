6d3bc318f9b0cf948a9ebec3565196c051e8c2be
szehao <ngszehao123+github@hotmail.com>
2025-04-10 12:05:36 +0800

    chore: remove caching for pools

--- src/app/_requests/poolWithDelegate.ts
+++ src/app/_requests/poolWithDelegate.ts
@@ -1,59 +1,49 @@
-import { unstable_cache } from 'next/cache'
-
 import { getKasuSDK } from '@/actions/getKasuSDK'
 import { getCurrentEpoch } from '@/app/_requests/currentEpoch'
 import { getPoolDelegate } from '@/app/_requests/poolDelegate'
 
 import { PoolOverviewWithDelegate } from '@/types/page'
 
-const CACHE_TTL = 60 * 60 // 1 hour
+// const CACHE_TTL = 60 * 60 // 1 hour
 
-export const getPoolWithDelegate = unstable_cache(
-  async (
-    poolId?: string,
-    activePools: boolean = true,
-    oversubscribed = false
-  ) => {
-    const sdk = await getKasuSDK()
-    const currentEpoch = await getCurrentEpoch()
+export const getPoolWithDelegate = async (
+  poolId?: string,
+  activePools: boolean = true,
+  oversubscribed = false
+) => {
+  const sdk = await getKasuSDK()
+  const currentEpoch = await getCurrentEpoch()
 
-    const [pools, poolDelegates] = await Promise.all([
-      sdk.DataService.getPoolOverview(currentEpoch),
-      getPoolDelegate(),
-    ])
+  const [pools, poolDelegates] = await Promise.all([
+    sdk.DataService.getPoolOverview(currentEpoch),
+    getPoolDelegate(),
+  ])
 
-    const poolsWithDelegate = pools
-      .filter((pool) => {
-        if (poolId) {
-          return pool.id === poolId
-        }
+  const poolsWithDelegate = pools
+    .filter((pool) => {
+      if (poolId) {
+        return pool.id === poolId
+      }
 
-        const showActivePools = activePools ? pool.isActive : !pool.isActive
+      const showActivePools = activePools ? pool.isActive : !pool.isActive
 
-        const showOversubscribedPools = oversubscribed
-          ? pool.isOversubscribed
-          : !pool.isOversubscribed
+      const showOversubscribedPools = oversubscribed
+        ? pool.isOversubscribed
+        : !pool.isOversubscribed
 
-        return showActivePools && showOversubscribedPools
-      })
-      .reduce((acc, cur) => {
-        const delegate = poolDelegates.find((delegate) =>
-          delegate.otherKASUPools.find((pool) => pool.id === cur.id)
-        )
+      return showActivePools && showOversubscribedPools
+    })
+    .reduce((acc, cur) => {
+      const delegate = poolDelegates.find((delegate) =>
+        delegate.otherKASUPools.find((pool) => pool.id === cur.id)
+      )
 
-        if (delegate) {
-          acc.push({ ...cur, delegate })
-        }
+      if (delegate) {
+        acc.push({ ...cur, delegate })
+      }
 
-        return acc
-      }, [] as PoolOverviewWithDelegate[])
+      return acc
+    }, [] as PoolOverviewWithDelegate[])
 
-    return poolsWithDelegate
-  },
-  ['poolsWithDelegate'],
-  {
-    // use it to revalidate/flush cache with revalidateTag()
-    tags: ['poolsWithDelegate'],
-    revalidate: CACHE_TTL,
-  }
-)
+  return poolsWithDelegate
+}
