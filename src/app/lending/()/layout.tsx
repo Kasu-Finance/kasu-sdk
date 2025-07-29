0f9e07079358e570776e0475e1a03bd1e713accf
szehao <ngszehao123+github@hotmail.com>
2025-07-29 14:32:30 +0800

    update: add lite mode app and movefrom layout to page

--- src/app/lending/()/layout.tsx
+++ src/app/lending/()/layout.tsx
@@ -1,14 +1,12 @@
-import { Container, Grid2, Stack, Typography } from '@mui/material'
+import { Container } from '@mui/material'
 import { ReactNode, Suspense } from 'react'
 
 import LiteModeRenderer from '@/components/atoms/LiteModeRenderer'
 import ReferralDetector from '@/components/atoms/ReferralDetector'
-import WaveBox from '@/components/atoms/WaveBox'
 import Home from '@/components/molecules/home'
 import HomeOverviewSkeleton from '@/components/molecules/loaders/home/HomeOverviewSkeleton'
 import HomeTabs from '@/components/organisms/home/HomeTabs'
 import PromoBanner from '@/components/organisms/lending/PromoBanner'
-import BasicStats from '@/components/organisms/lite/BasicStats'
 
 import HomeState from '@/context/home/home.provider'
 
@@ -22,27 +20,7 @@ export default async function LendingLayout({ children }: LendingLayoutProps) {
   return (
     <Container maxWidth='lg'>
       <LiteModeRenderer
-        renderOnLiteMode={
-          <Grid2 container spacing={3}>
-            <Grid2 size={8}>
-              <WaveBox variant='dark-gray' borderRadius={6} p={2}>
-                <Stack spacing={4.5}>
-                  <Typography variant='h2' color='gold.dark'>
-                    Lending Portfolio
-                  </Typography>
-                  <Stack spacing={3}>
-                    <BasicStats />
-                  </Stack>
-                </Stack>
-              </WaveBox>
-            </Grid2>
-            <Grid2 size={4}>
-              <WaveBox variant='dark-gray' borderRadius={6} p={2}>
-                <Typography color='white'>world</Typography>
-              </WaveBox>
-            </Grid2>
-          </Grid2>
-        }
+        renderOnLiteMode={children}
         otherwise={
           <HomeState>
             <Suspense fallback={<HomeOverviewSkeleton />}>
--- src/app/lending/()/page.tsx
+++ src/app/lending/()/page.tsx
@@ -6,8 +6,10 @@ import { Suspense } from 'react'
 import getTranslation from '@/hooks/useTranslation'
 
 import EmptyDataPlaceholder from '@/components/atoms/EmptyDataPlaceholder'
+import LiteModeRenderer from '@/components/atoms/LiteModeRenderer'
 import PoolLayoutWrapper from '@/components/organisms/home/PoolLayoutWrapper'
 import PoolLayoutWrapperSkeleton from '@/components/organisms/home/PoolLayoutWrapperSkeleton'
+import LiteModeApp from '@/components/organisms/lite'
 
 import { getCurrentEpoch } from '@/app/_requests/currentEpoch'
 import { getPoolWithDelegate } from '@/app/_requests/poolWithDelegate'
@@ -21,17 +23,22 @@ const LendingPage = async () => {
   ])
 
   return (
-    <Box mt={3}>
-      <Suspense fallback={<PoolLayoutWrapperSkeleton />}>
-        <PoolLayoutWrapper
-          pools={poolsWithDelegate}
-          currentEpoch={currentEpoch}
-          emptyPoolsPlaceholder={
-            <EmptyDataPlaceholder text={t('home.no-data.activePools')} />
-          }
-        />
-      </Suspense>
-    </Box>
+    <LiteModeRenderer
+      renderOnLiteMode={<LiteModeApp />}
+      otherwise={
+        <Box mt={3}>
+          <Suspense fallback={<PoolLayoutWrapperSkeleton />}>
+            <PoolLayoutWrapper
+              pools={poolsWithDelegate}
+              currentEpoch={currentEpoch}
+              emptyPoolsPlaceholder={
+                <EmptyDataPlaceholder text={t('home.no-data.activePools')} />
+              }
+            />
+          </Suspense>
+        </Box>
+      }
+    />
   )
 }
 
