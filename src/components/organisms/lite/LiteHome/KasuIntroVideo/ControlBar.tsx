abd0d40d41849c9bebaf57ed1e4edad140d5de13
szehao <ngszehao123+github@hotmail.com>
2025-09-10 11:29:38 +0800

    update: add control bar

--- src/components/organisms/lite/LiteHome/KasuIntroVideo/ControlBar.tsx
+++ src/components/organisms/lite/LiteHome/KasuIntroVideo/ControlBar.tsx
@@ -1,23 +1,53 @@
+import FullscreenIcon from '@mui/icons-material/Fullscreen'
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
 import PauseIcon from '@mui/icons-material/Pause'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
-import { Box, IconButton } from '@mui/material'
+import VolumeOffIcon from '@mui/icons-material/VolumeOff'
+import VolumeUpIcon from '@mui/icons-material/VolumeUp'
+import { Box, IconButton, Slider, Typography } from '@mui/material'
+
+import { VideoControls } from '@/components/organisms/lite/LiteHome/KasuIntroVideo'
+
+import { TimeConversions } from '@/utils'
 
 type ControlBarProps = {
-  controls: {
-    play: boolean
-    fullscreen: boolean
-    volume: number
-    time: number
-  }
+  controls: VideoControls
   handlePlay: () => void
   handlePause: () => void
+  toggleMute: () => void
+  handleVolumeChange: (e: Event, value: number | number[]) => void
+  toggleFullscreen: () => void
 }
 
 const ControlBar: React.FC<ControlBarProps> = ({
   controls,
   handlePause,
   handlePlay,
+  toggleMute,
+  handleVolumeChange,
+  toggleFullscreen,
 }) => {
+  const getTimeFormat = (totalSeconds: number) => {
+    const hours = Math.floor(totalSeconds / TimeConversions.SECONDS_PER_HOUR)
+    const minutes = Math.floor(
+      (totalSeconds % TimeConversions.SECONDS_PER_HOUR) /
+        TimeConversions.SECONDS_PER_MINUTE
+    )
+    const seconds = totalSeconds % TimeConversions.SECONDS_PER_MINUTE
+
+    const formattedHours = String(hours).padStart(2, '0')
+    const formattedMinutes = String(minutes).padStart(2, '0')
+    const formattedSeconds = String(seconds).padStart(2, '0')
+
+    const defaultFormat = `${formattedMinutes}:${formattedSeconds}`
+
+    if (hours <= 0) {
+      return defaultFormat
+    }
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
+  }
+
   return (
     <Box
       position='absolute'
@@ -25,19 +55,93 @@ const ControlBar: React.FC<ControlBarProps> = ({
       height={42}
       width='100%'
       display='flex'
+      justifyContent='space-between'
       alignItems='center'
       px={2}
       my={1}
     >
-      <Box>
+      <Box display='flex' alignItems='center' gap={1}>
         <IconButton
           onClick={controls.play ? handlePause : handlePlay}
           color='primary'
         >
           {controls.play ? <PauseIcon /> : <PlayArrowIcon />}
         </IconButton>
+        <Box
+          display='flex'
+          alignItems='center'
+          overflow='hidden'
+          sx={{
+            '&:hover, &:focus-within': {
+              '& > .MuiBox-root': {
+                width: 86,
+              },
+            },
+          }}
+        >
+          <IconButton color='primary' onClick={toggleMute}>
+            {controls.muted ? <VolumeOffIcon /> : <VolumeUpIcon />}
+          </IconButton>
+          <Box
+            width={0}
+            display='flex'
+            alignItems='center'
+            overflow='hidden'
+            sx={{
+              transition: 'width 0.2s ease-in',
+            }}
+          >
+            <Box px={2} width='100%' display='flex' alignItems='center'>
+              <Slider
+                aria-label='Volume'
+                color='primary'
+                value={controls.muted ? 0 : controls.volume}
+                onChange={handleVolumeChange}
+                min={0}
+                max={1}
+                step={0.01}
+                sx={{
+                  '.MuiSlider-thumb': {
+                    '&:hover': {
+                      boxShadow: '0px 0px 0px 8px rgba(196, 153, 108, 0.16)',
+                    },
+                    '&.Mui-active': {
+                      boxShadow: '0px 0px 0px 8px rgba(196, 153, 108, 0.16)',
+                    },
+
+                    '&::after': {
+                      width: 32,
+                      height: 32,
+                    },
+                  },
+                }}
+                slotProps={{
+                  thumb: {
+                    style: {
+                      width: 12,
+                      height: 12,
+                    },
+                  },
+                  track: {
+                    style: {
+                      height: 3,
+                    },
+                  },
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Typography variant='baseMd' color='gold.dark'>
+          {getTimeFormat(Math.floor(controls.currentTime))} /{' '}
+          {getTimeFormat(Math.floor(controls.duration))}
+        </Typography>
       </Box>
-      <Box></Box>
+      <Box>
+        <IconButton onClick={toggleFullscreen} color='primary'>
+          {controls.fullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+        </IconButton>
+      </Box>
     </Box>
   )
 }
--- src/components/organisms/lite/LiteHome/KasuIntroVideo/index.tsx
+++ src/components/organisms/lite/LiteHome/KasuIntroVideo/index.tsx
@@ -6,29 +6,83 @@ import ControlBar from '@/components/organisms/lite/LiteHome/KasuIntroVideo/Cont
 
 import { PlayIcon } from '@/assets/icons'
 
+export type VideoControls = {
+  play: boolean
+  fullscreen: boolean
+  volume: number
+  time: number
+  muted: boolean
+  currentTime: number
+  duration: number
+}
+
 const KasuIntroVideo = () => {
+  const videoContainerRef = useRef<HTMLDivElement>(null)
   const videoRef = useRef<HTMLVideoElement>(null)
 
-  const [controls, setControls] = useState({
+  const [controls, setControls] = useState<VideoControls>({
     play: false,
     fullscreen: false,
     volume: 0.5,
+    muted: false,
     time: 0,
+    currentTime: 0,
+    duration: 0,
   })
 
   useEffect(() => {
     const video = videoRef.current
+    const videoContainer = videoContainerRef.current
 
-    if (!video) return
+    if (!video || !videoContainer) return
+
+    videoContainer.addEventListener('fullscreenchange', () => {
+      setControls((prev) => ({
+        ...prev,
+        fullscreen: Boolean(document.fullscreenElement),
+      }))
+    })
+
+    video.addEventListener('durationchange', () => {
+      setControls((prev) => ({
+        ...prev,
+        duration: video.duration,
+      }))
+    })
 
     video.addEventListener('timeupdate', () => {
-      if (!video) return
+      setControls((prev) => ({
+        ...prev,
+        currentTime: video.currentTime,
+      }))
 
       if (video.currentTime === video.duration) {
         setControls((prev) => ({ ...prev, play: false }))
       }
     })
   }, [])
+
+  const handleVolumeChange = (e: Event, value: number | number[]) => {
+    if (!videoRef.current) return
+
+    const volume = Array.isArray(value) ? value[0] : value
+
+    videoRef.current.volume = volume
+
+    setControls((prev) => ({
+      ...prev,
+      volume,
+      muted: volume === 0 ? true : false,
+    }))
+  }
+
+  const toggleMute = () => {
+    if (!videoRef.current) return
+
+    videoRef.current.muted = !videoRef.current.muted
+
+    setControls((prev) => ({ ...prev, muted: !prev.muted }))
+  }
 
   const handlePlay = () => {
     if (!videoRef.current) return
@@ -46,6 +100,52 @@ const KasuIntroVideo = () => {
     videoRef.current.pause()
   }
 
+  const focusEventListener = (event: KeyboardEvent) => {
+    if (!videoContainerRef.current) return
+
+    // Note that "F" is case-sensitive (uppercase):
+    if (event.key === 'F') {
+      // Check if we're in fullscreen mode
+      if (document.fullscreenElement) {
+        document.exitFullscreen()
+        return
+      }
+      // Otherwise enter fullscreen mode
+      videoContainerRef.current
+        .requestFullscreen({ navigationUI: 'hide' })
+        .catch((err) => {
+          console.error(`Error enabling fullscreen: ${err.message}`)
+        })
+    }
+
+    if (event.key === 'Esc') {
+      if (document.fullscreenElement) {
+        document.exitFullscreen()
+        return
+      }
+    }
+  }
+
+  const handleFocus = () => {
+    document.addEventListener('keydown', focusEventListener)
+  }
+
+  const handleBlur = () => {
+    document.removeEventListener('keydown', focusEventListener)
+  }
+
+  const toggleFullscreen = () => {
+    if (!videoContainerRef.current) return
+
+    if (controls.fullscreen) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen()
+      }
+    } else {
+      videoContainerRef.current.requestFullscreen({ navigationUI: 'hide' })
+    }
+  }
+
   return (
     <Stack spacing={8} my={8}>
       <Grid2 container spacing={4} alignItems='center'>
@@ -61,7 +161,14 @@ const KasuIntroVideo = () => {
           <DottedDivider />
         </Grid2>
       </Grid2>
-      <Box borderRadius={3} overflow='hidden' position='relative'>
+      <Box
+        borderRadius={3}
+        overflow='hidden'
+        position='relative'
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        ref={videoContainerRef}
+      >
         <Box
           position='absolute'
           top={0}
@@ -70,31 +177,39 @@ const KasuIntroVideo = () => {
           height='100%'
           zIndex={1}
         >
-          <IconButton
-            sx={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }}
-            onClick={handlePlay}
-          >
-            <PlayIcon />
-          </IconButton>
+          {controls.currentTime === 0 && (
+            <IconButton
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+              }}
+              onClick={handlePlay}
+            >
+              <PlayIcon />
+            </IconButton>
+          )}
           <ControlBar
             controls={controls}
             handlePause={handlePause}
             handlePlay={handlePlay}
+            toggleMute={toggleMute}
+            handleVolumeChange={handleVolumeChange}
+            toggleFullscreen={toggleFullscreen}
           />
         </Box>
         <Box
           component='video'
           width='100%'
-          height={671}
+          height={controls.fullscreen ? '100%' : 671}
           ref={videoRef}
           borderRadius={3}
           sx={{
             objectFit: 'cover',
+            '&::-webkit-media-controls': {
+              display: 'none !important',
+            },
           }}
           controlsList='nodownload'
         >
