4570b77f024d080212a88aa340436f66ff8b016c
szehao <ngszehao123+github@hotmail.com>
2025-10-01 15:08:07 +0800

    update: handle boundary error logging

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,7 @@
 import { Stack, Typography } from '@mui/material'
 import Image from 'next/image'
 import { useEffect } from 'react'
+import { useAccount, useChainId } from 'wagmi'
 
 import Cat from '@/images/cat.png'
 
@@ -13,16 +14,35 @@ export default function Error({
   error: Error & { digest?: string }
   // reset: () => void
 }) {
+  const chainId = useChainId()
+  const { address } = useAccount()
+
   useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
+    fetch('/api/logging', {
+      body: JSON.stringify({
+        error: {
+          message: error.message,
+          cause: error.cause,
+          name: error.name,
+          stack: error.stack,
+        },
+        chainId,
+        address,
+        isBoundaryError: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      method: 'POST',
+    })
+  }, [error, address, chainId])
 
   return (
     <Stack mt={20} alignItems='center'>
       <Image src={Cat} alt='Cat' style={{ width: 548, height: 'auto' }} />
       <Typography variant='h3' color='gray.extraDark'>
-        Something went wrong! Please try again.
+        Currently under maintenance, please try again later.
       </Typography>
     </Stack>
   )
