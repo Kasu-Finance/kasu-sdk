ad0a2544403f0a4fbd7060b3095778b05b770d87
Kiril Ivanov <kivanov82@yahoo.com>
2025-11-19 14:38:17 +0100

    fix: don't use env var during the build

--- src/app/api/email-challenge/route.ts
+++ src/app/api/email-challenge/route.ts
@@ -1,17 +1,22 @@
 import { createSdk } from '@compilot/js-sdk'
 import { NextRequest, NextResponse } from 'next/server'
 
-const apiClient = createSdk({
-  apiKey: process.env.NEXERA_API_KEY || '',
-})
-
 const EMAIL_WORKFLOW = 'a95f52b0-7017-4745-ab92-4106b8e89e33'
+
+const getApiClient = () => {
+  const apiKey = process.env.NEXERA_API_KEY
+  if (!apiKey) {
+    throw new Error('NEXERA_API_KEY is not configured.')
+  }
+
+  return createSdk({ apiKey })
+}
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
 
-    const sessionRes = await apiClient.createWeb3Challenge({
+    const sessionRes = await getApiClient().createWeb3Challenge({
       workflowId: EMAIL_WORKFLOW,
       ...body,
     })
